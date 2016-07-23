e48d5a5004cc8fa16d02d9c57d189601109adf28
Josh LaBounty <joshua.labounty@stonybrook.edu>
2016-07-23 19:12:53 -0400

    Adds quick macro to get calibration factor for from B vs. I measurement

--- ShieldTest/get_calibration_factor.C
+++ ShieldTest/get_calibration_factor.C
@@ -1,7 +1,7 @@
 int get_calibration_factor()
 {
 
-	std::string file_path = "/home/josh/Dropbox/Stony\ Brook\ Research\ Team\ Folder/LabVIEW/DATA_Gaussmeter/DataFile_160722_122115.txt";
+	std::string file_path = "/home/josh/Dropbox/Stony\ Brook\ Research\ Team\ Folder/LabVIEW/DATA_Gaussmeter/DataFile_160722_123707.txt";
 	TTree *t = new TTree();
 	TCanvas *c = new TCanvas();
 	gStyle->SetOptFit();
@@ -10,6 +10,11 @@ int get_calibration_factor()
 	TF1 *fit1 = new TF1("fit1","pol1",0,25);
 	Graph->Fit("fit1");
 	c->Update();
+
+	double slope = fit1->GetParameter(1);
+
+	cout << endl << "The Calibration Factor for this setup is: " << slope << " mT/mV" << endl;
+	cout << "Consult the shunt resistor you are using for the mV<->A conversion factor (most commonly 1:1)" << endl;
 
 	return 0;
 }
